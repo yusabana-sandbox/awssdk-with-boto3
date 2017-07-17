b8b7415b862c084345b5953e329aeca43b1a7f90
Yuji Takaesu <taka.esu.user@gmail.com>
2017-07-17 10:48:03 +0900

    drop dynamodb table

---
+++ mydynamodb/movies6_droptable.py
@@ -0,0 +1,8 @@
+from __future__ import print_function
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+# dynamodb = boto3.resource('dynamodb', region_name='ap-northeast-1', endpoint_url='http://localhost:4569')
+
+table = dynamodb.Table('Movies')
+table.delete()
